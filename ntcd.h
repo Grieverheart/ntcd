6bd1175a11519975e8e28a1c4774570e845a8dbf
Grieverheart <nicktasios@gmail.com>
2017-04-19 12:07:40 +0200

    Fixed ntcd_gjk_raycast definition.

--- ntcd.h
+++ ntcd.h
@@ -11,10 +11,20 @@ typedef struct{
 
 int ntcd_gjk_boolean(const transform_t*, const void*, const transform_t*, const void*);
 void ntcd_gjk_distance(const transform_t*, const void*, const transform_t*, const void*, double* dist_vec);
-int ntcd_gjk_raycast(const transform_t*, const void*, const transform_t*, void*, const double* ray_dir, double* distance, double* normal);
+int ntcd_gjk_raycast(const transform_t*, const void*, const transform_t*, const void*, const double* ray_dir, double* distance, double* normal);
 
+//Test
+#if 0
+#define NTCD_IMPLEMENTATION
+#include "ntcd.h"
+
+int main(int argc, char* argv[]){
+    return 0;
+}
 #endif
+
+#endif //__NTCD_H__
 
 //Implementation
 #ifdef NTCD_IMPLEMENTATION
-#endif
+#endif //NTCD_IMPLEMENTATION
